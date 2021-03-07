f9e46b7cb8fdce434008b7df0837fcc72ec6da45
Alejandro E. Rendon <alejandro@rendon.co>
2021-03-06 20:01:01 -0500

    Added CSS framework

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,43 +7,26 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
+import { Container } from 'react-bootstrap';
 
-import Header from './header';
-import './layout.css';
+import SiteBar from './SiteBar';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
+      <SiteBar />
+      <Container>
+        <main>{children}</main>
+      </Container>
+      <footer
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          marginTop: `2rem`,
         }}
       >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
     </>
   );
 };
